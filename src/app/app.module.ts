44eab4c3fe39df5b38986b4045ca29f82a5c294f
cristian-simba <davidsimba200@gmail.com>
2024-05-31 16:32:08 -0500

    ubicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+// geolocation and native-geocoder
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+  providers: [
+    Geolocation,
+    NativeGeocoder,
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy
+    }
+  ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB5Je7xi2GSE4OE9QmaPo6KKVSexnJrXVs",
+    authDomain: "fir-auth-ce833.firebaseapp.com",
+    projectId: "fir-auth-ce833",
+    storageBucket: "fir-auth-ce833.appspot.com",
+    messagingSenderId: "324939252749",
+    appId: "1:324939252749:web:c01f199ed522bd15f2c144",
+    measurementId: "G-TKV1BTG9N6"
+  }  
 };
 
 /*
